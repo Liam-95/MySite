92e851ad1ea1232fd6b40f7045f2e17df73d841a
Liam Behar <liambehar@gmail.com>
2020-09-15 18:08:42 +0300

    update

--- liam/js/script.js
+++ liam/js/script.js
@@ -6,3 +6,14 @@ $(function () {
     }
     });
 });
+var switchMenuToActive = function () {
+    var classes = document.querySelector("#navHomeB").className;
+    classes = classes.replace(new RegExp("active","g"),"");
+    document.querySelector("#navHomeB").className = classes;
+    // add 'active'
+    classes = document.querySelector("#navMenuB").className;
+    if (classes.indexOf("active")== -1) {
+        classes +=" active";
+        document.querySelector("#navMenuB").className = classes;
+    }
+};
