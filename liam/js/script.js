73df262596eef56d9eeeb117ad087e41b7cdf593
Liam Behar <liambehar@gmail.com>
2020-09-16 20:17:44 +0300

    update

--- liam/js/script.js
+++ liam/js/script.js
@@ -1,19 +1,18 @@
 $(function () {
-    $("#navbarToggle").blur(function(event){
+    $("#navbarToggle").blur(function (event) {
         var screenWidth = window.innerWidth;
-        if(screenWidth<576){
         $("#collapsable-nav").collapse('hide');
-    }
+
     });
 });
 var switchMenuToActive = function () {
     var classes = document.querySelector("#navHomeB").className;
-    classes = classes.replace(new RegExp("active","g"),"");
+    classes = classes.replace(new RegExp("active", "g"), "");
     document.querySelector("#navHomeB").className = classes;
     // add 'active'
     classes = document.querySelector("#navMenuB").className;
-    if (classes.indexOf("active")== -1) {
-        classes +=" active";
+    if (classes.indexOf("active") == -1) {
+        classes += " active";
         document.querySelector("#navMenuB").className = classes;
     }
 };
